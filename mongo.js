82e10b740022944defa96c8f283fb8b0b68af1be
johninfant021 <127079706+johninfant021@users.noreply.github.com>
2024-02-19 18:44:14 +0530

    logbackend

--- mongo.js
+++ mongo.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://0.0.0.0:27017/authentication")
+  .connect(
+    "mongodb+srv://john-user17:Sakthi1721@cluster0.xlcyymi.mongodb.net/authentication"
+  )
   .then(() => {
     console.log("mongodb connected");
   })
